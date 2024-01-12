1907456e172c7f4bfdb3d2552ebe214a936db952
showk.mahmoud <showk.mahmoud@3ddx.com>
2024-01-13 03:30:07 +0400

    BF: starting in the form and the api

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { ChangeEvent, FC, PropsWithChildren, useState } from 'react'
+import { FormGroup, Input, Label } from 'reactstrap'
 
-function Login() {
+interface ILoginModal {
+  onSubmitForm?:any
+}
+const Login:FC<PropsWithChildren<ILoginModal>> = ({onSubmitForm}) => {
+  const getInitialValue = () => {
+    return {
+      name: "",
+      email: "",
+    };
+  };
+  const [formData, setFormData] = useState( getInitialValue());
+  const { name, email} = formData;
+  const handleFormDataChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    onSubmitForm(formData);
+  };
   return (
-    <div>Login</div>
+    <form className="px-4 py-5 mt-5" onSubmit={onSubmit}>
+    {/* user name */}
+    <FormGroup>
+      <Label for="name" className="text-capitalize ">
+        user name
+      </Label>
+      <Input
+        id="name"
+        name="name"
+        value={name}
+        onChange={handleFormDataChange}
+        type="text"
+      />
+    </FormGroup>
+    </form>
   )
 }
 
---
+++ src/shared/API/API.ts
@@ -0,0 +1,7 @@
+export const api='https://utaskmanager.onrender.com/';
+export const APIName = {
+    checkUSerName:'auth/check-userName',
+    checkEmail:'auth/check-email',
+    register:'auth/register',
+    login:'auth/login'
+}
